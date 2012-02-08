9da2a6f5496d60a71ee2e7be56d3c936804d1a2c
Will Brown <will.h.brown@gmail.com>
2012-02-08 16:45:36 -0500

    Layout work for edit pane.
    Buttons with dummy actions, background image, a sample image for panning and zooming (but no panning or zooming yet)

--- Annotate23D/ViewController.h
+++ Annotate23D/ViewController.h
@@ -10,4 +10,19 @@
 
 @interface ViewController : UIViewController
 
+-(IBAction)selectButton:(id)sender;
+-(IBAction)splineButton:(id)sender;
+-(IBAction)cuboidButton:(id)sender;
+-(IBAction)ellipseButton:(id)sender;
+-(IBAction)connectButton:(id)sender;
+-(IBAction)sameTiltButton:(id)sender;
+-(IBAction)sameSizeButton:(id)sender;
+-(IBAction)sameRadiusButton:(id)sender;
+-(IBAction)mirrorShapeButton:(id)sender;
+-(IBAction)alignShapeButton:(id)sender;
+-(IBAction)centerShapeButton:(id)sender;
+
+-(void)buttonClick:(NSString*)toolName;
+
+-(void)handlePan:(UIGestureRecognizer*)sender;
 @end
--- Annotate23D/ViewController.m
+++ Annotate23D/ViewController.m
@@ -12,33 +12,35 @@
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+  [super didReceiveMemoryWarning];
+  // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+  UIPanGestureRecognizer *panGestureRecognizer = 
+      [[UIPanGestureRecognizer alloc]
+       initWithTarget:self action:@selector(handlePan:)];
 }
 
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+  [super viewDidUnload];
+  // Release any retained subviews of the main view.
+  // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    [super viewWillAppear:animated];
+  [super viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [super viewDidAppear:animated];
+  [super viewDidAppear:animated];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -53,8 +55,67 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
-    return YES;
+  // Return YES for supported orientations
+  return YES;
+}
+
+- (void)handlePan:(UIGestureRecognizer *)sender {
+  NSLog(@"Detected pan gesture.");
+}
+
+- (void)buttonClick:(NSString*)toolName {
+  UIAlertView *message =
+  [[UIAlertView alloc]
+   initWithTitle:@"Tool clicked"
+   message:[NSString stringWithFormat:@"You selected the %@ tool.", toolName]
+   delegate:nil
+   cancelButtonTitle:@"Thanks!"
+   otherButtonTitles:nil];
+  [message show];
+}
+
+- (IBAction)selectButton:(id)sender {
+  [self buttonClick:@"select"];
+}
+
+-(IBAction)splineButton:(id)sender {
+  [self buttonClick:@"spline"];
+}
+
+-(IBAction)cuboidButton:(id)sender {
+  [self buttonClick:@"cuboid"];
+}
+
+-(IBAction)ellipseButton:(id)sender {
+  [self buttonClick:@"ellipse"];
+}
+
+-(IBAction)connectButton:(id)sender {
+  [self buttonClick:@"connection"];
+}
+
+-(IBAction)sameTiltButton:(id)sender {
+  [self buttonClick:@"same-tilt annotation"];
+}
+
+-(IBAction)sameSizeButton:(id)sender {
+  [self buttonClick:@"same-size annotation"];
+}
+
+-(IBAction)sameRadiusButton:(id)sender {
+  [self buttonClick:@"same-radius annotation"];
+}
+
+-(IBAction)mirrorShapeButton:(id)sender {
+  [self buttonClick:@"mirror shape"];
+}
+
+-(IBAction)alignShapeButton:(id)sender {
+  [self buttonClick:@"align shape"];
+}
+
+-(IBAction)centerShapeButton:(id)sender {
+  [self buttonClick:@"center shape"];
 }
 
 @end

dcd8292fdbae7e61e7224f3b8a22214a954a12bf
Will Brown <will.h.brown@gmail.com>
2012-02-08 14:58:03 -0500

    Initial Commit

---
+++ Annotate23D/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Annotate23D
+//
+//  Created by William Brown on 2012/02/08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Annotate23D/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Annotate23D
+//
+//  Created by William Brown on 2012/02/08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
